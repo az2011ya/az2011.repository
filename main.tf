f121b0ccdbcb573ac10c0875d7a1d68aab29670f
Yacine A <az2011ya@hotmail.fr>
2025-02-02 02:44:51 +0100

    add aws-launch-template and autoscaling_group : depl Config Web Server

--- main.tf
+++ main.tf
@@ -37,3 +37,60 @@ output "public_ip" {
  description = "The public IP address of the web server"
 }
 
+resource "aws_launch_template" "example" {
+   name     = "example-launch-template"
+   image_id = "ami-0fb653ca2d3203ac1"
+   instance_type = "t2.micro"
+   # security_groups = [aws_security_group.instance.id]
+   
+
+   # Configurer les interfaces réseau
+   network_interfaces {
+     associate_public_ip_address = true
+     security_groups             = [aws_security_group.instance.id]
+  }
+ 
+   user_data = base64encode(<<EOF
+             #!/bin/bash
+             echo "Hello, World" > index.html
+             nohup busybox httpd -f -p ${var.server_port} &
+             EOF
+             )
+
+   # Required when using a launch configuration with an auto scaling group.
+   lifecycle {
+     create_before_destroy = true
+   }
+
+}
+
+resource "aws_autoscaling_group" "example" {
+   #launch_configuration = aws_launch_configuration.example.name
+   vpc_zone_identifier = data.aws_subnets.default.ids   
+  # desired_capacity     = 1
+   min_size = 2
+   max_size = 10 
+ 
+   tag {
+     key = "Name"
+     value = "terraform-asg-example"
+     propagate_at_launch = true
+  }
+
+   launch_template {
+    name = aws_launch_template.example.name
+    version             = "$Latest"
+  }
+}
+
+
+data "aws_vpc" "default" {
+ default = true
+}
+
+data "aws_subnets" "default" {
+ filter {
+ name = "vpc-id"
+ values = [data.aws_vpc.default.id]
+ }
+}
